a537646ea46c6d3ad7d446086c88dccd5220c190
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2022-12-24 00:32:51 -0600

    Add time-block color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // Add code to display the current date in the header of the page.
-
-  // Convert cardinal number (e.g. 1, 2, 3, 4) to an ordinal number (1st, 2nd, 3rd, 4th)
-  function ordinal(n) {
-    var s = ["th", "st", "nd", "rd"];
-    var v = n%100;
-    return n + (s[(v-20)%10] || s[v] || s[0]);
-  };
-
   // Map weekday indices to weekday names
   const DAYS = {
     0: "Sunday",
@@ -39,15 +13,63 @@ $(function () {
     6: "Saturday",
   };
 
+  // Convert cardinal number (e.g. 1, 2, 3, 4) to an ordinal number (1st, 2nd, 3rd, 4th)
+  function ordinal(n) {
+    var s = ["th", "st", "nd", "rd"];
+    var v = n%100;
+    return n + (s[(v-20)%10] || s[v] || s[0]);
+  };
+
   // Get current day information
   var today = dayjs();
   var weekdayIndex = today.day(); // Gets day as a number - 0 (Sunday) to 6 (Saturday)
   var weekdayName = DAYS[weekdayIndex]; // Changes day number to day name
   var month = today.format("MMMM");
+  var currentHour = today.format("H");
   var dayOfMonth = today.format("D");
   var ordinalDayOfMonth = ordinal(dayOfMonth);
 
-  // Add day information to HTML document
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+  // Maps hour with time block element ID
+  const TIME_BLOCK_IDS = {
+    9: "#hour-9",
+    10: "#hour-10",
+    11: "#hour-11",
+    12: "#hour-12",
+    13: "#hour-13",
+    14: "#hour-14",
+    15: "#hour-15",
+    16: "#hour-16",
+    17: "#hour-17",
+  }
+  
+  // Add past, present, or future class to each time block
+  for (var hourIdx = 9; hourIdx < 18; hourIdx++) {
+    // Grab time-block element
+    var elementID = TIME_BLOCK_IDS[hourIdx];
+    var timeBlockElement = $(elementID);
+
+    // Assign class to time-block element
+    if (currentHour > hourIdx) {
+      timeBlockElement.addClass("past");
+    } else if (currentHour == hourIdx) {
+      timeBlockElement.addClass("present");
+    } else if (currentHour < hourIdx) {
+      timeBlockElement.addClass("future");
+    }
+  }
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+
+  // Add code to display the current date in the header of the page.
   var dateString = weekdayName + ", " + month + " " + ordinalDayOfMonth
   $('#currentDay').text(dateString);
 });
