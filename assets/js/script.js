af73e91e1d2d127fe377a1d8d1eddf1c9afd3b67
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2022-12-24 10:58:06 -0600

    Add save button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,26 @@ $(function () {
   var dayOfMonth = today.format("D");
   var ordinalDayOfMonth = ordinal(dayOfMonth);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Holds time block text
+  var timeBlockValues = {
+    "hour-9": '',
+    "hour-10": '',
+    "hour-11": '',
+    "hour-12": '',
+    "hour-13": '',
+    "hour-14": '',
+    "hour-15": '',
+    "hour-16": '',
+    "hour-17": '',
+  }
+
+  // Save text area text to local storage on save button click
+  $('.saveBtn').on('click', function() {
+    var timeBlockID = this.parentElement.id;
+    var textAreaValue = this.previousElementSibling.value;
+    timeBlockValues[timeBlockID] = textAreaValue;
+    window.localStorage.setItem('scheduleItems', JSON.stringify(timeBlockValues));
+  });
 
   // Maps hour with time block element ID
   const TIME_BLOCK_IDS = {
@@ -68,6 +82,7 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
 
   // Add code to display the current date in the header of the page.
   var dateString = weekdayName + ", " + month + " " + ordinalDayOfMonth
